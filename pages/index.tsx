5b30fbb08468c51921b4d8599adaeb4287d5077a
Vaishnavi <chvkarmy@gmail.com>
2023-02-11 10:23:14 +0530

    Added image and font family in tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import iconCart from "../public/images/image-product-desktop.jpg";
 
 export default function Home() {
   return (
@@ -11,7 +9,8 @@ export default function Home() {
       <Head>
         <title>Sample App</title>
       </Head>
-      <p className="text-lg">Sample text in lg</p>
+      <p className="text-lg font-montserrat">Sample text in lg</p>
+      <Image src={iconCart} alt={""}></Image>
     </>
   );
 }
